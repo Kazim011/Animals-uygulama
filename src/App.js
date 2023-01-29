76c26964036c7edd3ba51286677c8f0331b9a99a
kazım <kazimnergiz123@gmail.com>
2023-01-29 16:25:10 +0300

    guncelleme

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
+console.log(getRandomAnimal());
+
+function App() {
+  const [animals, setAnimals] = useState([]);
+
+  const HandleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
+  return (
+    <div className="app">
+      <button onClick={HandleClick}>Add Animal</button>
+      <div className="animal-list"> {renderedAnimals} </div>
+    </div>
+  );
+}
+
+export default App;
